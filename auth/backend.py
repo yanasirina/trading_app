e6fc6a5b9735c03bbfc4b1b74b1dbc2d7e0cc495
yanasirina <yanasirina0@yandex.ru>
2024-02-10 14:47:13 +0500

    добавила вью для аутентификации

--- auth/backend.py
+++ auth/backend.py
@@ -1,4 +1,4 @@
-from fastapi_users.authentication import CookieTransport, JWTStrategy, AuthenticationBackend
+from fastapi_users.authentication import JWTStrategy, AuthenticationBackend, BearerTransport
 
 from config import JWT_SECRET
 
@@ -7,10 +7,10 @@ def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=JWT_SECRET, lifetime_seconds=60*60)
 
 
-cookie_transport = CookieTransport(cookie_name='token', cookie_max_age=60*60)
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
 auth_backend = AuthenticationBackend(
     name="jwt",
-    transport=cookie_transport,
+    transport=bearer_transport,
     get_strategy=get_jwt_strategy,
 )
---
+++ auth/schemas.py
@@ -0,0 +1,16 @@
+from fastapi_users import schemas
+
+
+class UserRead(schemas.BaseUser[int]):
+    username: str
+    role_id: int
+
+
+class UserCreate(schemas.BaseUserCreate):
+    username: str
+    role_id: int
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    username: str | None = None
+    role_id: int | None = None
