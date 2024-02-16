84171deaeb74128ed273a38be713c6e85344927f
yanasirina <yanasirina0@yandex.ru>
2024-02-16 17:00:18 +0500

    эндпойнты для операций

---
+++ src/operations/router.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import get_async_session
+from operations.models import Operation
+from operations.schemas import OperationCreate
+
+
+router = APIRouter(
+    prefix="/operations",
+    tags=["Operation"]
+)
+
+
+@router.get("/")
+async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
+    query = select(Operation).where(Operation.type == operation_type)
+    result = await session.execute(query)
+    return result.mappings().all()
+
+
+@router.post("/")
+async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    stmt = insert(Operation).values(**new_operation.dict())
+    await session.execute(stmt)
+    await session.commit()
+    return {"status": "success"}
