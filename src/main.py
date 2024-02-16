d19436879246ab7f7ae39a43e80d7755e38fb9d0
yanasirina <yanasirina0@yandex.ru>
2024-02-16 17:16:58 +0500

    добавила механизмм кэширования

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
+import time
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from redis import asyncio as aioredis
 
 from auth.router import router as auth_router
 from operations.router import router as operation_router
@@ -9,6 +15,19 @@ app = FastAPI(title="Trading App")
 
 app.include_router(auth_router)
 app.include_router(operation_router)
+
+
+@app.get("/long_operation")
+@cache(expire=60)
+async def index():
+    time.sleep(4)
+    return dict(hello="world")
+
+
+@app.on_event("startup")
+async def startup():
+    redis = aioredis.from_url("redis://localhost")
+    FastAPICache.init(RedisBackend(redis), prefix="trading-app-cache")
 
 
 if __name__ == "__main__":
